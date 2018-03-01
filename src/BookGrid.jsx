3decc4fcbde73f48fd855d3b25738df45c2ae70b
utv <uthangv@gmail.com>
2018-03-01 11:01:37 -0800

    init Shelves and BookGrid

--- src/BookGrid.jsx
+++ src/BookGrid.jsx
@@ -1,21 +1,30 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-class Bookshelf extends React.Component {
+class BookGrid extends React.Component {
   static propTypes = {
-    title: PropTypes.string.isRequired
+    books: PropTypes.array.isRequired,
+    shelf: PropTypes.string
   }
 
   render() {
+    const { books, shelf } = this.props
+
     return (
-      <div className="bookshelf">
-        <h2 className="bookshelf-title">Currently Reading</h2>
-        <div className="bookshelf-books">
-          <ol className="books-grid">
-            <li>
+      <ol className="books-grid">
+        {
+          books.filter(book => book.shelf === shelf).map(book => (
+            <li key={book.id}>
               <div className="book">
                 <div className="book-top">
-                  <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: 'url("http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api")' }}></div>
+                  <div className="book-cover"
+                    style={{
+                      width: 128,
+                      height: 193,
+                      backgroundImage: `url(${book.imageLinks.thumbnail})`
+                    }}
+                  >
+                  </div>
                   <div className="book-shelf-changer">
                     <select>
                       <option value="none" disabled>Move to...</option>
@@ -26,14 +35,15 @@ class Bookshelf extends React.Component {
                     </select>
                   </div>
                 </div>
-                <div className="book-title">To Kill a Mockingbird</div>
+                <div className="book-title">{}</div>
                 <div className="book-authors">Harper Lee</div>
               </div>
             </li>
-
-          </ol>
-        </div>
-      </div>
+          ))
+        }
+      </ol>
     )
   }
 }
+
+export default BookGrid
---
+++ src/Shelves.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import BookGrid from './BookGrid'
+
+class Shelves extends React.Component {
+  static propTypes = {
+    books: PropTypes.array.isRequired
+  }
+
+  render() {
+    const { books } = this.props
+    const shelves = [
+      { id: "wantToRead", title: "Want to Read" },
+      { id: "currentlyReading", title: "Currently Reading" },
+      { id: "read", title: "Want to Read" },
+      { id: "none", title: "None" }
+    ]
+    return (
+      <div className="list-books-content">
+        <div>
+          {
+            shelves.filter(shelf => shelf.id !== "none").map(shelf => (
+              <div key={shelf.id} className="bookshelf">
+                <h2 className="bookshelf-title">Currently Reading</h2>
+                <div className="bookshelf-books">
+                  <BookGrid books={books} shelf={shelf.id} />
+                </div>
+              </div>
+            ))
+          }
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Shelves
